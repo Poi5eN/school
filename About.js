f2a4ae1c38ebaa4408fb6b78c066d511c5bd6e1f
Poi5eN <111029389+Poi5eN@users.noreply.github.com>
2023-05-08 08:34:05 +0530

    add files

---
+++ About.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // })
+
+    let myStyle = {
+        color: props.mode === 'dark'?'white':'#042743',
+        backgroundColor: props.mode === 'dark'?'rgb(36 74 104)':'white',
+    }
+
+
+    return (
+        <div classNameName="container" style={{color: props.mode === 'dark'?'white':'#042743'}}>
+            <h1 classNameName="my-3">About Us</h1>
+            <div className="accordion" id="accordionExample" style={myStyle}>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Analyze your Test
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>TextMod is a smart tool.</strong> TexMod gives you a way to analyze your text quickly and efficiently. Be it word count, character count and several others.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item" style={myStyle}>
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Free To Use
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>TextMod is a free character count tool</strong> that provides instant character count and word count statistics for a given text. 
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Browser Compatible
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div className="accordion-body" style={myStyle}>
+                            <strong>TextMod works in any web browsers</strong> such as Chrome, Firefox, Internet Explorer,etc. It works efficiently in any given device.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
